48c20dd9e17f5863c61f41f4533403bf4d2a3c9d
5Suminsss2 <sumin.kim.dev@gmail.com>
2024-11-12 14:45:38 +0900

    #Setting: tailwindCss 설치

--- planIt/src/components/todo/TodoBox.tsx
+++ planIt/src/components/todo/TodoBox.tsx
@@ -2,6 +2,7 @@ const TodoBox = () => {
   return (
     <>
       <section>
+        <div className="text-3xl font-bold underline">Hello world!</div>
         <div>날짜</div>
         <div>못한 todo 팝업</div>
         <div>루틴팝업</div>
