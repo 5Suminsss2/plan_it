c773f0243c32d7d21b6555322a59d0e9f8fe44b7
5Suminsss2 <sumin.kim.dev@gmail.com>
2025-05-22 18:04:29 +0900

    #Feat: progressCalendar > 달력 기능 구현중
        - 특정 날짜 클릭할 경우, 클릭한 시간이 찍힌 event bar 노출

--- planIt/src/containers/ProgressCalendarContainer.tsx
+++ planIt/src/containers/ProgressCalendarContainer.tsx
@@ -1,24 +1,58 @@
 import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid"; // 먼저 import 필요
+import { EventContentArg } from "@fullcalendar/core";
+import dayjs from "dayjs";
 
 export default function ProgressCalendarContainer() {
   const [events, setEvents] = useState([
     { title: "처음 일정", date: "2025-05-21" },
   ]);
 
-  const handleDateClick = (arg: any) => {
-    const newEvent = { title: "새 일정", date: arg.dateStr };
+  const handleDateClick = (arg: DateClickArg) => {
+    // 등록한 현재 시각 가져오기
+    const now = dayjs();
+    const hourStr = now.hour().toString().padStart(2, "0");
+    const minuteStr = now.minute().toString().padStart(2, "0");
+
+    const newEvent = {
+      title: `${hourStr}:${minuteStr} \n DAY2 18/20 \n 🦄`,
+      date: arg.dateStr,
+      backgroundColor: "#ff7171",
+      borderColor: "white",
+      textColor: "white",
+      extendedProps: {
+        location: "회의실 A",
+        description: "팀 주간 회의",
+        category: "단어시험",
+      },
+    };
+
     setEvents([...events, newEvent]);
+  };
+
+  // \n -> <div> 태그로 변경
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    const lines = eventInfo.event.title.split("\n");
+
+    return (
+      <div style={{ padding: "4px 6px" }}>
+        {lines.map((line: string, index: number) => (
+          <div key={index}>{line}</div>
+        ))}
+      </div>
+    );
   };
 
   return (
     <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
+      plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
       initialView="dayGridMonth"
       dateClick={handleDateClick}
       events={events}
+      eventContent={renderEventContent}
     />
   );
 }
