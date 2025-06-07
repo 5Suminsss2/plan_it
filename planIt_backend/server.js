74c9937ac52330714cad8ad6909b3526dd4a6e36
5Suminsss2 <sumin.kim.dev@gmail.com>
2025-06-08 04:10:18 +0900

    #Fix: backend > front 배포 문제 수정 : 바인딩딩

--- planIt_backend/server.js
+++ planIt_backend/server.js
@@ -20,4 +20,6 @@ app.use(express.json());
 app.use("/api", routes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Listening on port ${PORT}`);
+});
