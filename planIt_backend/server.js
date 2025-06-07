cebdd8024c08b2054f15a0f5236e596c23150f28
5Suminsss2 <sumin.kim.dev@gmail.com>
2025-06-08 01:49:20 +0900

    #Setting: backend > mongodb prod 버전 연결하도록 env 수정정

--- planIt_backend/server.js
+++ planIt_backend/server.js
@@ -1,15 +1,22 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+// NODE_ENV에 따라 불러올 env 파일 결정
+const env = process.env.NODE_ENV || "development"; // 기본은 development
+const envFile = `.env.${env}`;
+dotenv.config({ path: envFile });
+
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const routes = require("./routes");
 
 const app = express();
 connectDB();
 
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use("/api", routes);
 
 const PORT = process.env.PORT || 5000;
