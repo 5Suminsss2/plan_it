ff7213168e339d45a987eae575d4332a21ef6ec8
5Suminsss2 <sumin.kim.dev@gmail.com>
2025-06-08 04:31:52 +0900

    #Fix: frontend > 배포 오류 수정

--- planIt/vite.config.ts
+++ planIt/vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 내부적으로 0.0.0.0 의미
+  },
 });
